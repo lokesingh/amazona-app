4faece43a9f807693e3ceda23e116c98315966e9
lokesingh <lokendra.thoughtwin@gmail.com>
2020-12-16 19:19:06 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import data from './data';
-
+import { BrowserRouter,Link,Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <div className="grid-container">
         <header className="row">
@@ -16,39 +18,14 @@ function App() {
           </div>
         </header>
         <main>
-          <div>
-            <div className="row center">
-              {
-               data.products.map((product) => (
-                  <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-
-                      <img className="medium" src={`.${product.image}`} alt="product" />
-                    </a>
-                    <div className="card-body">
-                      <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                      </a>
-                      <div className="rating">
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                      </div>
-                      <div className="price">${product.price}</div>
-                    </div>
-                  </div>
-               ))
-              }
-
-
-            </div>
-          </div>
+        <Route path="/product/:id" component={ProductScreen} exact></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+         
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
