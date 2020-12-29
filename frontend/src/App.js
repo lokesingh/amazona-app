dfa48a5aca2786f9edf7f9e2beabf9583f3fe5de
lokesingh <lokendra.thoughtwin@gmail.com>
2020-12-29 12:49:46 +0530

    sign in api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,64 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter, Link, Route } from "react-router-dom";
-import CartScreen from "./screens/CartScreen";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  console.log('userInfo',userInfo)
+  const dispatch = useDispatch();
+  
+  const signoutHandler = () => {
+   dispatch(signout());
+  };
   return (
     <BrowserRouter>
-      <div className="App">
-        <div className="grid-container">
-          <header className="row">
-            <div>
-              <Link className="brand" to="/">
-                amazona
-              </Link>
-            </div>
-            <div>
-              <Link to="/cart">
-                Cart
-                {cartItems.length > 0 && (
-                  <span className="badge">{cartItems.length}</span>
-                )}
-              </Link>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
               <Link to="/signin">Sign In</Link>
-            </div>
-          </header>
-          <main>
-            <Route path="/cart/:id?" component={CartScreen}></Route>
-            <Route path="/product/:id" component={ProductScreen} exact></Route>
-            <Route path="/" component={HomeScreen} exact></Route>
-          </main>
-          <footer className="row center">All right reserved</footer>
-        </div>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,14 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
 import { cartReducer } from './reducers/cardReducers';
 import { productDetailsReducer, productListReducers } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
+
 const initialState = {
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')
+          ? JSON.parse(localStorage.getItem('userInfo'))
+          : null,
+      },
     cart:{
         cartItems:localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')): []
     }
@@ -12,8 +19,10 @@ const initialState = {
 const reducer = combineReducers({
     productList:productListReducers,
     productDetails: productDetailsReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    userSignin:userSigninReducer
 })
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer,initialState,composeEnhancer(applyMiddleware(thunk)))
 
