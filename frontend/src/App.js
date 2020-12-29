622fe179ebf59354baf29f1bbade3a9bf798a174
lokesingh <lokendra.thoughtwin@gmail.com>
2020-12-29 18:46:17 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
@@ -47,7 +48,7 @@ function App() {
                     </Link>
                   </li>
                 </ul>
-              </div>
+              </div> 
             ) : (
               <Link to="/signin">Sign In</Link>
             )}
@@ -58,6 +59,7 @@ function App() {
           <Route path="/product/:id" exact component={ProductScreen}></Route>
           <Route path="/signin" exact component={SigninScreen}></Route>
           <Route path="/register"  component={RegisterScreen}></Route>
+          <Route path="/shipping"  component={ShippingAddressScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
