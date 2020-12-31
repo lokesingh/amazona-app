4d43071d9cd4a77c14bbbb5aff4dedd531a83a28
lokesingh <lokendra.thoughtwin@gmail.com>
2020-12-31 16:18:14 +0530

    h

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ const initialState = {
     cart:{
         cartItems:localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')): [],
         shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')): {}
+        ,paymentMethod: 'PayPal'
     }
 };
 
